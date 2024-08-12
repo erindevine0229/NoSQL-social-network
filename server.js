1b3a65aa0faefe2e83d5f5e691dcc6e072a8b71f
Erin DeVine <erinj291@gmail.com>
2024-08-12 19:35:52 -0400

    filled out connection file and basics of server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+});
