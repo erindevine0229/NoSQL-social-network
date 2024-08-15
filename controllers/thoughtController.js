cbf056bec3b9208fa5585f0e70a5c2dc5fa7f7ab
Erin DeVine <erinj291@gmail.com>
2024-08-14 23:19:53 -0400

    added get and post controllers for thoughts. Need to finish PUT and DELETE

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,51 @@
-const Thought = require('../models/Thought');
+const { User, Thought } = require('../models');
+
+module.exports = {
+
+async getAllThoughts(req, res) {
+    try {
+        const allThoughts = await Thought.find();
+        res.json(allThoughts)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+
+async getThoughtById(req, res) {
+    try { 
+        const thought = await Thought.findOne({ _id: req.params.thoughtId })
+        if (!thought) {
+            return res.status(400).json({ message: "Unable to locate thought with that ID" })
+        }
+        res.json( { thought });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+
+async createNewThought(req, res) {
+    try {
+        const thought = await Thought.create(req.body);
+        const user = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $push: {thoughts: thought._id } },
+            { new: true }
+        )
+        res,json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+async updateThoughtById(req,res) {
+
+},
+
+
+async deleteThoughtById(req, res) {
+    
+},
+
+};
