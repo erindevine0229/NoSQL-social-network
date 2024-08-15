dc4dde2aee466ed7ce326d87e4f8d60cf6afe7bb
Erin DeVine <erinj291@gmail.com>
2024-08-15 19:23:28 -0400

    finished thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -30,12 +30,12 @@ async getThoughtById(req, res) {
 async createNewThought(req, res) {
     try {
         const thought = await Thought.create(req.body);
-        const user = await User.findOneAndUpdate(
+        await User.findOneAndUpdate(
             { _id: req.body.userId },
             { $push: {thoughts: thought._id } },
             { new: true }
         )
-        res,json(thought);
+        res.json(thought);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -75,13 +75,35 @@ async deleteThoughtById(req, res) {
 
 async addNewReaction(req, res) {
     try {
-        const 
+        const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body }},
+            { runValidators: true, new: true }
+        );
+        if (!thought) {
+            return res.status(404).json({ message: "Unable to locate thought with that ID"});
+        }
+        res.json(thought);
+    } catch (err) {
+        res.status(500).json(err);
     }
 },
 
 
 async deleteExistingReaction(req, res) {
-
+    try {
+        const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions : { reactionId: req.params.reactionId }}},
+            { runValidators: true, new: true }
+        );
+        if (!thought) {
+            res.status(404).json({ message: "Unable to locate thought with this ID" });
+        }
+        res.json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 },
 
 };
